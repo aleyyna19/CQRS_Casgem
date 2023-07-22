83d8a74f93298d1a9cb6ddfaefa6de1186f767ca
Monster <Monster@MONSTER>
2023-07-22 15:54:50 +0300

    Add project files.

---
+++ CQRS_Casgem/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS_Casgem.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = MONSTER\\SQLEXPRESS; initial catalog=CasgemmCQRSDb;integrated Security=true");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
